93fe74ff3fd6ddf1c5a72575932f0abc3f4c70ad
TheodorusArie <theodorusarie@hotmail.com>
2020-07-02 17:35:43 +0700

    routing

--- router/index.js
+++ router/index.js
@@ -3,26 +3,39 @@ const ControllerMahasiswa = require('../Controllers/ControllerMahasiswa')
 const mataPelajaranRoute = require('./mataPelajaran')
 const mahasiswaRouter = require('./mahasiswa')
 const qr = require('qr-image')
-const { dashboard } = require('../Controllers/ControllerHome')
+//const { dashboard } = require('../Controllers/ControllerHome')
 
 const route = require('express').Router()
 
 
-route.get('/',ControllerHome.findHome)
-route.get('/register',ControllerMahasiswa.register)
-route.post('/register',ControllerMahasiswa.registerPost)
-route.use('/MataPelajaran',mataPelajaranRoute)
-route.use('/Mahasiswa',mahasiswaRouter)
-route.get('/login',ControllerHome.login)
-route.post('/login',ControllerHome.loginPost)
-route.get('/dashboardMahasiswa/:id',ControllerHome.dashboard)
-route.get('/logout',ControllerHome.logOut)
-route.get('/qr/:text/:id',function(req,res){
-    let params=req.params.text;
+route.get('/', ControllerHome.findHome)
+route.get('/register', ControllerMahasiswa.register)
+route.post('/register', ControllerMahasiswa.registerPost)
+route.use('/MataPelajaran', mataPelajaranRoute)
+route.use('/Mahasiswa', mahasiswaRouter)
+route.get('/login', ControllerHome.login)
+route.post('/login', ControllerHome.loginPost)
+route.get('/dashboardMahasiswa/:id', (req, res, next) => {
+    if (req.session.isLogin) {
+        next();
+    }
+    else {
+        let pesan = `Harus Log in untuk melihat Dashboard Mahasiswa`;
+        res.redirect(`/login?pesan=${pesan}`)
+    }
+
+
+}, ControllerHome.dashboard)
+
+
+
+route.get('/logout', ControllerHome.logOut)
+route.get('/qr/:text/:id', function (req, res) {
+    let params = req.params.text;
     let id = req.params.id;
-    let gabunganStr =`/${params}/${id}`;
-    let code = qr.image(gabunganStr,{type:'png',size:10});
-    res.setHeader('Content-type','image/png');
+    let gabunganStr = `/${params}/${id}`;
+    let code = qr.image(gabunganStr, { type: 'png', size: 10 });
+    res.setHeader('Content-type', 'image/png');
     code.pipe(res);
 })
 
--- router/mataPelajaran.js
+++ router/mataPelajaran.js
@@ -3,15 +3,17 @@ const ControllerMataPelajaran = require('../Controllers/ControllerMataPelajaran'
 const ControllerHome = require('../Controllers/ControllerHome');
 
 const mataPelajaranRoute = require('express').Router()
-mataPelajaranRoute.get('/', (req, res, next) => {
-    if (req.session.isLogin) {
-        next();
-    }
-    else {
-        let pesan = `Harus Log in untuk melihat Mata Pelajaran`;
-        res.redirect(`/login?pesan=${pesan}`)
-    }
-}, ControllerMataPelajaran.listMataPelajaran);
+mataPelajaranRoute.get('/', 
+// (req, res, next) => {
+//     if (req.session.isLogin) {
+//         next();
+//     }
+//     else {
+//         let pesan = `Harus Log in untuk melihat Mata Pelajaran`;
+//         res.redirect(`/login?pesan=${pesan}`)
+//     }
+// }, 
+ControllerMataPelajaran.listMataPelajaran);
 mataPelajaranRoute.get('/add', ControllerMataPelajaran.mataPelajaranAdd)
 mataPelajaranRoute.post('/add', ControllerMataPelajaran.mataPelajaranAddPost)
 mataPelajaranRoute.get('/addSKS/:id',ControllerMataPelajaran.mataPelajaranSKS)
